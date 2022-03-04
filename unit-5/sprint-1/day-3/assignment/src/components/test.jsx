099ff2227e4a35ae1baf6ff8bd56d633007cd887
Niranjan <narayana9492324464@gmail.com>
2022-03-04 16:17:08 +0530

    day 3 check

---
+++ unit-5/sprint-1/day-3/assignment/src/components/test.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function Test(){
+    const [counter , setCounter] = useState(0)
+
+    const changer = (value) => {
+
+        setCounter(counter+value)
+    }
+    return (
+        <div>
+            <h1> count is : {counter}</h1>
+            <div className="button_div">
+                <button onClick={ () => {
+                    changer(1)
+                } }> Add By 1</button>
+                <button onClick={ () => {
+                    if(counter<1){
+                        return counter*0
+                    }
+                    changer(-1)
+                } }> Remove By 1</button>
+            </div>
+
+        </div>
+    )
+}
+
+export {Test}
