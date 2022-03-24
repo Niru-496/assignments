183bb329a4774e91129f6a347e24df5cd463d4f7
Niranjan <narayana9492324464@gmail.com>
2022-03-24 17:53:56 +0530

    small changes

--- unit-5/sprint-3/evaluation/c3/src/Components/Navbar/Navbar.jsx
+++ unit-5/sprint-3/evaluation/c3/src/Components/Navbar/Navbar.jsx
@@ -11,12 +11,9 @@ const links=[
 export const Navbar = () => {
   return (
     <>
-      <div className="navbar" style={{
-        display: "flex",justifyContent:"space-between",
-        backgroundColor:"red"
-      }}>
+      <div className="navbar">
         {/* Populate 5 buttons with EXACT same classnames as of their routes name */}
-        {/* Example: 
+        {/* Example:
             <button className="history"> Link to history here  </button>  */}
         {/* Home component will have `/` route and classname as `home`  */}
         <button className="home">
