9e0387f9dc719c8671bed49cbffd0d74822167c4
Niranjan <narayana9492324464@gmail.com>
2022-03-31 18:37:13 +0530

    api

--- unit-5/sprint-3/day-1/assignments/context-api/src/components/btn.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { CartContext } from "../context/cartcontext"
-
-export const Button = () => {
-    const { handleChange } = useContext(CartContext);
-    return <button onClick={() => {
-        handleChange(1)
-    }}> Add to cart</button>
-}
