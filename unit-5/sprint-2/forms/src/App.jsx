60c371a95e865e8dfc8eb48acd0b400456327cb6
Niranjan <narayana9492324464@gmail.com>
2022-03-31 18:33:26 +0530

    forms

---
+++ unit-5/sprint-2/forms/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Employe } from "./components/EmployeeDetails";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Employe />
+    </div>
+  );
+}
+
+export default App;
